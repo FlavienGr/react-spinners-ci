92a118fc0b4948a63f88b0a668630048a6f96eaf
Flavor85 <flaviengr75@gmail.com>
2019-05-23 18:44:24 +0200

    add storybook

--- src/Ring/Ring.js
+++ src/Ring/Ring.js
@@ -1,8 +1,8 @@
 import React from "react";
-import styles from "./styles.css";
+require("./styles.css");
+
 const Ring = () => (
   <>
-    <style>{styles}</style>
     <div className="lds-ring">
       <div />
       <div />
